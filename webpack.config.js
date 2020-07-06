11ffe11a3bfa63e8e701aaf610f87f1c0aaad767
jacket <1762982273@qq.com>
2020-07-06 22:58:34 +0800

    feat: 🎸 webpack
    add the basic webapck config

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+    mode: "production",
+    entry: {
+        index: './lib/index.tsx'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        library: 'tspry',
+        libraryTarget: "umd"
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            loader: 'awesome-typescript-loader'
+        }]
+    },
+}
