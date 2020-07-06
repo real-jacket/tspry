4f4b59ba03449089ea9e98fdf9195c858ba6fdd6
jacket <1762982273@qq.com>
2020-07-06 23:54:31 +0800

    feat: 🎸 webpack-dev-server、htmlplugin
    add webapck-dev-server and htmlplugin

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,6 @@
-console.log('hello world')
+console.log('hello world ++++++ ---')
+
+const div = document.createElement('div')
+div.innerText = 'this is a div...'
+
+document.body.appendChild(div)
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,32 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    mode: "production",
-    entry: {
-        index: './lib/index.tsx'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        library: 'tspry',
-        libraryTarget: "umd"
-    },
-    module: {
-        rules: [{
-            test: /\.tsx?$/,
-            loader: 'awesome-typescript-loader'
-        }]
-    },
+	mode: 'development',
+	entry: {
+		index: './lib/index.tsx',
+	},
+	devServer: {
+		contentBase: './dist',
+		hot: true,
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		library: 'tspry',
+		libraryTarget: 'umd',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'awesome-typescript-loader',
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'tspry ui',
+			template: 'index.html',
+		}),
+	],
 }
