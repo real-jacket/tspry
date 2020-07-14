1bbe8e07e105ebdc3df0bac5eea379cfa3056f40
jacket <1762982273@qq.com>
2020-07-14 22:17:10 +0800

    feat: 🎸 icon
    batch import icons

--- lib/Icon.tsx
+++ lib/Icon.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import '../icons/wechat.svg'
-import '../icons/alipay.svg'
-import '../icons/qq.svg'
+// import '../icons/wechat.svg'
+// import '../icons/alipay.svg'
+// import '../icons/qq.svg'
+import './importIcons'
 
 interface IconProps {
 	name: string
