e42f832be4674997dc4feeb5413554b364794b07
jacket <1762982273@qq.com>
2020-07-30 00:05:56 +0800

    test: 💍 coverage
    add coverage and reports of jest test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,19 @@
 module.exports = {
 	bail: 1,
 	verbose: true,
+	collectCoverage: true,
+	collectCoverageFrom: ['lib/**/*.{ts,tsx}', '!**/node_modules/**'],
+	coverageDirectory: 'coverage',
+	coverageReporters: ['lcov', 'text'],
+	reporters: ['default', ['jest-junit', { outputDirectory: './test-results/jest', outputName: 'results.xml' }]],
+	coverageThreshold: {
+		global: {
+			branches: 90,
+			functions: 95,
+			lines: 95,
+			statements: 95,
+		},
+	},
 	preset: 'ts-jest/presets/js-with-ts',
 	testMatch: ['<rootDir>/**/__tests__/**/*.test.(js|jsx|ts|tsx)'],
 	moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
