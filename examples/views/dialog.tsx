f32d8c046f2be46e6cc10c4df7eae7c9a9aaa578
jacket <1762982273@qq.com>
2020-08-12 18:01:49 +0800

    refactor: 💡 react
    update react to 17.0-rc

--- examples/views/dialog.tsx
+++ examples/views/dialog.tsx
@@ -3,31 +3,17 @@ import Dialog from '../../lib/dialog/dialog'
 
 export default function Dashboard() {
 	const [show, setShow] = useState(false)
+	const ok: React.MouseEventHandler = (e) => {
+		setShow(false)
+		console.log(e)
+	}
+	const cancel = () => {
+		setShow(false)
+	}
 	return (
 		<div>
 			<button onClick={() => setShow(!show)}>click</button>
-			<Dialog
-				visible={show}
-				onClose={() => {
-					setShow(false)
-				}}
-				buttons={[
-					<button
-						onClick={() => {
-							setShow(false)
-						}}
-					>
-						确定
-					</button>,
-					<button
-						onClick={() => {
-							setShow(false)
-						}}
-					>
-						取消
-					</button>,
-				]}
-			>
+			<Dialog visible={show} maskClosable={true} onOk={ok} onCancel={cancel}>
 				hi
 			</Dialog>
 		</div>
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,12 +1,15 @@
-import React, { Fragment, ReactElement } from 'react'
+import React, { ReactElement } from 'react'
 import { scopedClassMaker } from '../helpers/classes'
 import './dialog.scss'
-import '../index.scss'
 
 interface Props {
 	visible: Boolean
 	buttons?: Array<ReactElement>
-	onClose: React.MouseEventHandler
+	onClose?: React.MouseEventHandler
+	maskClosable?: Boolean
+	closable?: Boolean
+	onOk: React.MouseEventHandler
+	onCancel: React.MouseEventHandler
 }
 
 const scopedClass = scopedClassMaker('tspry-dialog')
@@ -14,26 +17,44 @@ const sc = scopedClass
 
 const Dialog: React.FunctionComponent<Props> = (props) => {
 	const close: React.MouseEventHandler = (e) => {
-		props.onClose(e)
+		props.onClose ? props.onClose(e) : props.onCancel(e)
+	}
+	const clickMask: React.MouseEventHandler = (e) => {
+		props.maskClosable && close(e)
+	}
+
+	const ok: React.MouseEventHandler = (e) => {
+		props.onOk(e)
+	}
+
+	const cancel: React.MouseEventHandler = (e) => {
+		props.onCancel(e)
 	}
 	return props.visible ? (
-		<Fragment>
-			<div className={sc('mask')}></div>
+		<>
+			<div className={sc('mask')} onClick={clickMask}></div>
 			<div className={sc()}>
-				<div className={sc('close')} onClick={close}>
-					x
-				</div>
+				{props.closable && (
+					<div className={sc('close')} onClick={close}>
+						x
+					</div>
+				)}
 				<header className={sc('header')}>提示</header>
 				<main className={sc('main')}>{props.children}</main>
-				{props.buttons && props.buttons.length > 0 && (
-					<footer className={sc('footer')}>
-						{props.buttons.map((button, index) => {
+				<footer className={sc('footer')}>
+					{props.buttons && props.buttons.length > 0 ? (
+						props.buttons.map((button, index) => {
 							return React.cloneElement(button, { key: index })
-						})}
-					</footer>
-				)}
+						})
+					) : (
+						<>
+							<button onClick={ok}> 确定 </button>
+							<button onClick={cancel}> 取消 </button>
+						</>
+					)}
+				</footer>
 			</div>
-		</Fragment>
+		</>
 	) : null
 }
 
