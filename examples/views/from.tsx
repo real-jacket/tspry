3b6d14cb7c7f3a0928f84a72c9abfc386a229343
jacket <1762982273@qq.com>
2021-02-04 23:11:58 +0800

    feat: 🎸 form

--- examples/views/from.tsx
+++ examples/views/from.tsx
@@ -1,18 +1,35 @@
 import React, { useState } from 'react'
-import Form from '../../lib/form/form'
+import Form, { Field, Rule, Error, Validator } from '../../lib/form/form'
 
 const FormDemo: React.FunctionComponent = () => {
-  const [formData] = useState({
+  const [formData, setFormData] = useState({
     username: '',
     password: '',
   })
 
-  const [fields] = useState([
+  const onChange = (key: string, value: string) => {
+    setFormData({
+      ...formData,
+      [key]: value,
+    })
+  }
+
+  const [fields] = useState<Array<Field>>([
     { name: 'username', label: '用户名', input: { type: 'text' } },
     { name: 'password', label: '密码', input: { type: 'password' } },
   ])
 
-  const submit = () => {}
+  const [rules] = useState<Array<Rule>>([
+    { key: 'username', required: true },
+    { key: 'password', min: 10, max: 16, required: true },
+  ])
+
+  const [errors, setErrors] = useState<Error[]>([])
+
+  const submit = () => {
+    setErrors(Validator(formData, rules))
+    if (errors.length > 0) return
+  }
 
   return (
     <>
@@ -22,6 +39,9 @@ const FormDemo: React.FunctionComponent = () => {
         fields={fields}
         buttons={<button>提交</button>}
         onSubmit={submit}
+        onChange={onChange}
+        rules={rules}
+        errors={errors}
       ></Form>
     </>
   )
--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -1,19 +1,107 @@
-import React from 'react'
+import React, { ChangeEvent, FormEvent, FormEventHandler } from 'react'
 
-interface FormProps {
-  value: { [key: string]: any }
-  fields: Array<{ name: string; label: string; input: { type: string } }>
+export interface Rule {
+  key: string
+  required?: boolean
+  min?: number
+  max?: number
+}
+
+export interface Field {
+  name: string
+  label: string
+  input: { type: string }
+}
+
+export interface FormData {
+  [key: string]: any
+}
+
+export interface FormProps {
+  value: FormData
+  fields: Array<Field>
   buttons: React.ReactFragment
+  onChange: (key: string, val: string) => void
   onSubmit: React.FormEventHandler
+  rules: Array<Rule>
+  errors: Array<Error>
+}
+
+function isMin(val: number | string, min: number): boolean {
+  if (typeof val === 'string') {
+    return val.length < min
+  } else {
+    return val.toString().length < min
+  }
+}
+
+function isMax(val: number | string, max: number): boolean {
+  if (typeof val === 'string') {
+    return val.length > max
+  } else {
+    return val.toString().length > max
+  }
+}
+
+const isEmpty = (val: any): boolean =>
+  val === '' || val === null || val === undefined
+
+export interface Error {
+  key: string
+  message: string
+}
+
+export const Validator = (
+  formData: FormData,
+  rules: Array<Rule>
+): Array<Error> => {
+  const errors: Array<Error> = []
+  rules.map((rule) => {
+    const { key } = rule
+    const val = formData[key]
+    if (rule.required) {
+      if (isEmpty(val)) {
+        errors.push({
+          key,
+          message: '请输入内容',
+        })
+      } else {
+        if (rule.min && isMin(formData[key], rule.min)) {
+          errors.push({
+            key,
+            message: '长度不得小于最小值',
+          })
+        }
+        if (rule.max && isMax(formData[key], rule.max)) {
+          errors.push({
+            key,
+            message: '长度不得大于最大值',
+          })
+        }
+      }
+    }
+  })
+  return errors
 }
 
 const Form: React.FunctionComponent<FormProps> = (props) => {
+  const { onSubmit, fields, errors, onChange } = props
+
+  const submit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    onSubmit(e)
+  }
+
   return (
-    <form>
-      {props.fields.map((f) => (
+    <form onSubmit={submit}>
+      {fields.map((f) => (
         <div key={f.name}>
           {f.label}
-          <input type={f.input.type}></input>
+          <input
+            type={f.input.type}
+            onChange={(e) => onChange(f.name, e.target.value)}
+          ></input>
+          <span>{errors.find((item) => item.key === f.name)?.message}</span>
         </div>
       ))}
       <div>{props.buttons}</div>
